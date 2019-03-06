2ddceb7d9c55e62c17124d9045ffdac7d5e04207
JustinBeaudry <beaudry.justin@gmail.com>
2019-03-05 22:21:17 -0800

    rewrites Token in es6 with import/export syntax

--- runtime/JavaScript/src/antlr4/Token.js
+++ runtime/JavaScript/src/antlr4/Token.js
@@ -1,151 +1,253 @@
-/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
+/**
+ * @copyright
+ * Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
  * Use of this file is governed by the BSD 3-clause license that
  * can be found in the LICENSE.txt file in the project root.
  */
-//
-
-// A token has properties: text, type, line, character position in the line
-// (so we can ignore tabs), token channel, index, and source from which
-// we obtained this token.
-
-function Token() {
-	this.source = null;
-	this.type = null; // token type of the token
-	this.channel = null; // The parser ignores everything not on DEFAULT_CHANNEL
-	this.start = null; // optional; return -1 if not implemented.
-	this.stop = null; // optional; return -1 if not implemented.
-	this.tokenIndex = null; // from 0..n-1 of the token object in the input stream
-	this.line = null; // line=1..n of the 1st character
-	this.column = null; // beginning of the line at which it occurs, 0..n-1
-	this._text = null; // text of the token.
-	return this;
-}
-
-Token.INVALID_TYPE = 0;
-
-// During lookahead operations, this "token" signifies we hit rule end ATN state
-// and did not follow it despite needing to.
-Token.EPSILON = -2;
-
-Token.MIN_USER_TOKEN_TYPE = 1;
-
-Token.EOF = -1;
-
-// All tokens go to the parser (unless skip() is called in that rule)
-// on a particular "channel". The parser tunes to a particular channel
-// so that whitespace etc... can go to the parser on a "hidden" channel.
-
-Token.DEFAULT_CHANNEL = 0;
-
-// Anything on different channel than DEFAULT_CHANNEL is not parsed
-// by parser.
-
-Token.HIDDEN_CHANNEL = 1;
-
-// Explicitly set the text for this token. If {code text} is not
-// {@code null}, then {@link //getText} will return this value rather than
-// extracting the text from the input.
-//
-// @param text The explicit text of the token, or {@code null} if the text
-// should be obtained from the input along with the start and stop indexes
-// of the token.
-
-Object.defineProperty(Token.prototype, "text", {
-	get : function() {
+/**
+ *
+ * @type "<EOF>"
+ * @constant
+ */
+const EOF = '<EOF>';
+/**
+ *
+ * @type "<no text>"
+ * @constant
+ */
+const NO_TEXT = '<no text>';
+/**
+ *
+ * @property {array} source
+ * @property {string} type - token type.
+ * @property {string} channel - the parser ignores everything not on
+ * DEFAULT_CHANNEL.
+ * @property {?number} start - optional; return -1 if not implemented.
+ * @property {?number} stop - optional; return -1 if not implemented.
+ * @property {number} tokenIndex - from 0..n-1 of the token object in the
+ * input stream.
+ * @property {number} line - 1..n of the 1st character
+ * @property {number} column - beginning of the line in which it occurs, 0..n-1
+ * @property {string} _text - text of the token
+ *
+ */
+class Token {
+	/**
+	 *
+	 * @param {array} source
+	 * @param {string} type
+	 * @param {string} channel
+	 * @param {number} start
+	 * @param {number} stop
+	 * @param {number} tokenIndex
+	 * @param {number} line
+	 * @param {number} column
+	 * @param {string} text
+	 * @returns {Token}
+	 */
+	constructor(
+		source=null,
+		type=null,
+		channel=null,
+		start=null,
+		stop=null,
+		tokenIndex=null,
+		line=null,
+		column=null,
+		text=null
+	) {
+		this.source = source;
+		this.type = type;
+		this.channel = channel;
+		this.start = start;
+		this.stop = stop;
+		this.tokenIndex = tokenIndex;
+		this.line = line;
+		this.column = column;
+		this._text = text;
+		this[Symbol.toStringTag] = 'Token';
+		return this;
+	}
+	/**
+	 *
+	 * @returns 0
+	 */
+	static get INVALID_TYPE() {
+		return 0;
+	}
+	/**
+	 *
+	 * @description
+	 * During lookahead operations, this "token" signifies we hit rule end ATN
+	 * state and did not follow it despite needing to.
+	 *
+	 * @returns -2
+	 */
+	static get EPSILON() {
+		return -2;
+	}
+	/**
+	 *
+	 * @returns 1
+	 */
+	static get MIN_USER_TOKEN_TYPE() {
+		return 1;
+	}
+	/**
+	 *
+	 * @returns -1
+	 */
+	static get EOF() {
+		return -1;
+	}
+	/**
+	 *
+	 * @description
+	 * All tokens go to the parser (unless skip() is called in that rule) on a
+	 * particular "channel". The parser tunes to a particular channel so that
+	 * whitespace etc. can go to the parser on a "hidden" channel.
+	 *
+	 * @returns 0
+	 */
+	static get DEFAULT_CHANNEL() {
+		return 0;
+	}
+	/**
+	 *
+	 * @description
+	 * Anything on a different channel that DEFAULT_CHANNEL is not parsed by
+	 * parser.
+	 *
+	 * @returns 1
+	 */
+	static get HIDDEN_CHANNEL() {
+		return 1;
+	}
+	get text() {
 		return this._text;
-	},
-	set : function(text) {
+	}
+	set text(text) {
 		this._text = text;
 	}
-});
-
-Token.prototype.getTokenSource = function() {
-	return this.source[0];
-};
-
-Token.prototype.getInputStream = function() {
-	return this.source[1];
-};
-
-function CommonToken(source, type, channel, start, stop) {
-	Token.call(this);
-	this.source = source !== undefined ? source : CommonToken.EMPTY_SOURCE;
-	this.type = type !== undefined ? type : null;
-	this.channel = channel !== undefined ? channel : Token.DEFAULT_CHANNEL;
-	this.start = start !== undefined ? start : -1;
-	this.stop = stop !== undefined ? stop : -1;
-	this.tokenIndex = -1;
-	if (this.source[0] !== null) {
-		this.line = source[0].line;
-		this.column = source[0].column;
-	} else {
-		this.column = -1;
+	getTokenSource() {
+		return this.source[0];
 	}
-	return this;
+	getInputStream() {
+		return this.source[1];
+	}
 }
 
-CommonToken.prototype = Object.create(Token.prototype);
-CommonToken.prototype.constructor = CommonToken;
-
-// An empty {@link Pair} which is used as the default value of
-// {@link //source} for tokens that do not have a source.
-CommonToken.EMPTY_SOURCE = [ null, null ];
-
-// Constructs a new {@link CommonToken} as a copy of another {@link Token}.
-//
-// <p>
-// If {@code oldToken} is also a {@link CommonToken} instance, the newly
-// constructed token will share a reference to the {@link //text} field and
-// the {@link Pair} stored in {@link //source}. Otherwise, {@link //text} will
-// be assigned the result of calling {@link //getText}, and {@link //source}
-// will be constructed from the result of {@link Token//getTokenSource} and
-// {@link Token//getInputStream}.</p>
-//
-// @param oldToken The token to copy.
-//
-CommonToken.prototype.clone = function() {
-	var t = new CommonToken(this.source, this.type, this.channel, this.start,
-			this.stop);
-	t.tokenIndex = this.tokenIndex;
-	t.line = this.line;
-	t.column = this.column;
-	t.text = this.text;
-	return t;
-};
-
-Object.defineProperty(CommonToken.prototype, "text", {
-	get : function() {
-		if (this._text !== null) {
+class CommonToken extends Token {
+	/**
+	 *
+	 * @param {array} source
+	 * @param {string} type
+	 * @param {string} channel
+	 * @param {number} start
+	 * @param {number} stop
+	 */
+	constructor(
+		source=CommonToken.EMPTY_SOURCE,
+		type=null,
+		channel=Token.DEFAULT_CHANNEL,
+		start=-1,
+		stop=-1
+	) {
+		let line = -1;
+		let column = -1;
+		if (source[0] != null) {
+			line = source[0].line;
+			column = source[0].column;
+		}
+		super(
+			source,
+			type,
+			channel,
+			start,
+			stop,
+			-1, // tokenIndex
+		);
+		this[Symbol.toStringTag] = 'CommonToken';
+		return this;
+	}
+	static FromObject({
+		source,
+		type,
+		channel,
+		start,
+		stop
+	}) {
+		return new CommonToken(source, type, channel, start, stop);
+	}
+	/**
+	 *
+	 * @description
+	 * An empty {@link Pair} which is used as the default value of
+	 * {@link //source} for tokens that do not have a source.
+	 *
+	 * @returns {[null, null]}
+	 */
+	static get EMPTY_SOURCE() {
+		return [null, null];
+	}
+	/**
+	 *
+	 * @description
+	 * Constructs a new {@link CommonToken} as a copy of another {@link Token}.
+	 *
+	 * <p>
+	 *   {@link //text} will be assigned the result of calling {@link //getText},
+	 *   and {@link //source} will be constructed from the result of
+	 *   {@link Token//getTokenSource} and {@link Token//getInputStream}
+	 * </p>
+	 *
+	 * @returns {CommonToken}
+	 */
+	clone() {
+		const {tokenIndex, line, column, text} = this;
+		let token = CommonToken.FromObject(this);
+		token.tokenIndex = tokenIndex;
+		token.line = line;
+		token.column = column;
+		token.text = text;
+		return token;
+	}
+	get text() {
+		if (this._text != null) {
 			return this._text;
 		}
-		var input = this.getInputStream();
-		if (input === null) {
+		let input = this.getInputStream();
+		if (input == null) {
 			return null;
 		}
-		var n = input.size;
-		if (this.start < n && this.stop < n) {
+		const size = input.size;
+		if (this.start < size && this.stop < size) {
 			return input.getText(this.start, this.stop);
 		} else {
-			return "<EOF>";
+			return EOF;
 		}
-	},
-	set : function(text) {
-		this._text = text;
 	}
-});
-
-CommonToken.prototype.toString = function() {
-	var txt = this.text;
-	if (txt !== null) {
-		txt = txt.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t");
-	} else {
-		txt = "<no text>";
+	toString() {
+		let text = this.text;
+		if (text == null) {
+			text = NO_TEXT;
+		} else {
+			// escape newline, carriage return, and tab
+			text = text
+				.replace(/\n/g, '\\n')
+				.replace(/\r/g, '\\r')
+				.replace(/\t/g, '\\t');
+		}
+		const {tokenIndex, start, stop, type, channel, line, column} = this;
+		let channelText = '';
+		if (channel > 0) {
+			channelText = `,channel=${channel},`;
+		}
+		return `[@${tokenIndex},${start}:${stop}='${text}',<${type}>${channelText},${line}:${column}]`;
 	}
-	return "[@" + this.tokenIndex + "," + this.start + ":" + this.stop + "='" +
-			txt + "',<" + this.type + ">" +
-			(this.channel > 0 ? ",channel=" + this.channel : "") + "," +
-			this.line + ":" + this.column + "]";
-};
+}
 
-exports.Token = Token;
-exports.CommonToken = CommonToken;
+export {
+	Token,
+	CommonToken
+}
